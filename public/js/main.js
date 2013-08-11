dd9c2dcf7e897940a7586e274130078369214a13
etai <etai@empeeric.com>
2013-08-11 14:53:47 +0300

    gallery slider

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,26 @@
+$.fn.hoverSlider = function(o) {
+    o = $.extend({}, $.fn.hoverSlider.settings, o);
+    var t = this.wrapInner('<div class="inner" />'),
+        width = t.width()/* - o.buffer*/,
+        inner = $('.inner', t),
+        slides = inner.children(),
+        y = o.slideWidth * slides.length;
+
+    inner.css('width', y);
+
+    t.on('mousemove', function(e) {
+        var x = e.clientX - t.offset().left,
+            offset = (x / width) * (y - width)/* - o.buffer*/;
+        inner.css('-webkit-transform', 'translateX(-'+offset+'px)');
+        //            t.scrollLeft(offset);
+    });
+    return this;
+};
+$.fn.hoverSlider.settings = {
+    slideWidth: 100,
+    buffer: 10
+};
+
 var router = function(cb, t) {
     var w = $(window).on('hashchange', function() {
         var params = location.hash.split('/').slice(1);
