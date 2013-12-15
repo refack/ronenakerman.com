84411ee85e236344a04a2097373eb54c3655e780
Refael Ackermann <refael@empeeric.com>
2013-12-15 21:01:06 +0200

    Scroller

--- public/js/main.js
+++ public/js/main.js
@@ -5,30 +5,3 @@ var router = function(cb, t) {
     });
     if (t) w.trigger('hashchange');
 };
-
-$.fn.hoverSlider = function(o) {
-    o = $.extend({}, $.fn.hoverSlider.settings, o);
-
-    var xy = o.orientation.toUpperCase(),
-        prop = xy == 'X' ? 'width' : 'height',
-        dir = xy == 'X' ? 'left' : 'top';
-
-    var el = this.wrapInner('<div class="inner" />'),
-        inner = $('.inner', el),
-        slides = inner.children(),
-        size = el[prop](),
-        innerSize = o.slideSize * slides.length;
-
-    inner.css(prop, innerSize);
-
-    el.on('mousemove', function(e) {
-        var x = e['client'+xy] - el.offset()[dir],
-            offset = (x / size) * (innerSize - size);
-        inner.css('-webkit-transform', 'translate'+xy+'(-'+offset+'px)');
-    });
-    return this;
-};
-$.fn.hoverSlider.settings = {
-    slideSize: 100,
-    orientation: 'x'
-};
