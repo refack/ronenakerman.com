6b78b55ef81de350bef38cf3f44b07bc59e49fd8
Refael Ackermann <me@refack.com>
2016-09-30 15:35:40 -0400

    new error with toString

--- server/models/galleries.js
+++ server/models/galleries.js
@@ -22,9 +22,11 @@ var schema = new mongoose.Schema({
     show: { type: Boolean, 'default': true }
 });
 
+/*
 schema.methods.toString = function(){
     return this.title;
 };
+*/
 
 schema.pre('save', function(next) {
     var url = this.url;
