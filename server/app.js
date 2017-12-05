e722626aa1dbca71200308ce3e85bbf7be74bc94
Refael Ackermann <refack@gmail.com>
2017-12-05 15:46:35 -0500

    Bump version & support more mime entries

--- server/app.js
+++ server/app.js
@@ -1,55 +1,59 @@
-var express = require('express'),
-    http = require('http'),
-    path = require('path'),
-    mongoose = require('mongoose'),
-    stylus = require('stylus'),
-    formage = require('formage');
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const mongoose = require('mongoose')
+const stylus = require('stylus')
+const formage = require('formage')
 
-require('./dust');
+require('./dust')
 
-var app = express();
+const app = express()
 
-app.set('site', 'Ronen Akerman');
-app.set('port', process.env.PORT || 80);
-app.set('mongo', process.env.MONGOLAB_URI || 'mongodb://localhost/ronenakerman');
-app.set('cloudinary', process.env.CLOUDINARY_URL);
+app.set('site', 'Ronen Akerman')
+app.set('port', process.env.PORT || 80)
+app.set('mongo', process.env.MONGOLAB_URI || 'mongodb://localhost/ronenakerman')
+app.set('cloudinary', process.env.CLOUDINARY_URL)
 
-app.engine('dust', require('consolidate').dust);
-app.set('view engine', 'dust');
-app.set('views', path.join(__dirname, '..', 'views'));
+app.engine('dust', require('consolidate').dust)
+app.set('view engine', 'dust')
+app.set('views', path.join(__dirname, '..', 'views'))
 app.use(stylus.middleware({
-    src: path.join(__dirname, '..', 'public'),
-    compile: function compile(str, path) {
-        return stylus(str)
-            .set('filename', path)
-            .use(require('nib')());
-    }
-}));
+  src: path.join(__dirname, '..', 'public'),
+  compile: function compile (str, path) {
+    return stylus(str)
+      .set('filename', path)
+      .use(require('nib')())
+  }
+}))
 
-
-app.configure('development', function () {
-    app.use(express.logger('dev'));
-});
-app.use(express.compress());
-app.use(express.cookieParser('aafa34nafksd'));
-app.use(express.cookieSession({ cookie: { maxAge: 60 * 60 * 24 * 10}, key: app.get('site')}));
-app.use(express.static(path.join(__dirname, '../public')));
-
-mongoose.connect(app.get('mongo'));
-formage.init(app, express, require('./models'), {
-    username: 'ronen',
-    password: 'lialia45',
-    title: 'Ronen Akerman'
-});
-
-app.use(app.router);
-require('./routes')(app);
-
-// development
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+if ('development' === app.get('env')) {
+  app.use(express.logger('dev'))
 }
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.use(express.compress())
+app.use(express.cookieParser('aafa34nafksd'))
+app.use(express.cookieSession({
+  cookie: {maxAge: 60 * 60 * 24 * 10},
+  key: app.get('site')
+}))
+app.use(express.static(path.join(__dirname, '../public')))
+
+mongoose.Promise = global.Promise
+mongoose.connect(app.get('mongo'), {useMongoClient: true})
+formage.init(app, express, require('./models'), {
+  username: 'ronen',
+  password: 'lialia45',
+  title: 'Ronen Akerman'
+})
+
+app.use(app.router)
+require('./routes')(app)
+
+// development
+if ('development' === app.get('env')) {
+  app.use(express.errorHandler())
+}
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'))
+})
